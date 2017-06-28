206b42c20c3a11d14fa501de7f464fc5cbe5228d
guonima <504117608@qq.com>
2017-06-28 23:07:47 +0800

    增加页面框架

--- src/router.js
+++ src/router.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Router, Route } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
-function RouterConfig({ history }) {
-  return (
-    <Router history={history}>
-      <Route path="/" component={IndexPage} />
-    </Router>
-  );
+function RouterConfig({history}) {
+    return (
+        <Router history={history}>
+    	  <Route path="/" component={IndexPage} />
+   		</Router>
+    );
 }
 
 export default RouterConfig;
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
+import { Layout } from 'antd';
+import SiderCustom from '../components/SiderCustom';
+import HeaderCustom from '../components/HeaderCustom';
 import styles from './IndexPage.css';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-      </ul>
-    </div>
-  );
-}
+const {Content, Footer} = Layout;
 
-IndexPage.propTypes = {
-};
+
+class IndexPage extends Component {
+    state = {
+        collapsed: false,
+    };
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+    render() {
+        return (
+            <Layout className="ant-layout-has-sider">
+              <SiderCustom path={this.props.location.pathname} collapsed={this.state.collapsed} />
+              <Layout>
+                <HeaderCustom toggle={this.toggle} />
+                <Content style={{
+                margin: '0 16px',
+                overflow: 'initial'
+            }}>
+                  {this.props.children}
+                </Content>
+                <Footer style={{
+                textAlign: 'center'
+            }}>
+                  React-Admin ©2017 Created by 865470087@qq.com
+                </Footer>
+              </Layout>
+            </Layout>
+        );
+    }
+}
 
 export default connect()(IndexPage);
