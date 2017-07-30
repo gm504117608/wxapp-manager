d2792a1614cc298d6f9639aaaa4bd8d131986fe0
guonima <504117608@qq.com>
2017-07-30 23:10:31 +0800

    菜单增加

--- src/components/SiderLayout/SiderLayout.js
+++ src/components/SiderLayout/SiderLayout.js
@@ -19,20 +19,47 @@ class SiderLayout extends Component {
         return ( 
             <Sider trigger={null} className={styles.sider} collapsible collapsed={this.state.collapsed}>
                 <Menu theme="white" mode="inline" defaultSelectedKeys={['1']}>
-                <SubMenu key="system" title={<span><Icon type="mail" /><span>系统管理</span></span>}>
-                    <MenuItem key="user"><Link to="/index">用户信息</Link></MenuItem>
-                    <MenuItem key="role"><Link to="/login">角色管理</Link></MenuItem>
-                </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>菜单二</span></span>}>
-                    <MenuItem key="3"><Link to="/dashboard">菜单二1</Link></MenuItem>
-                    <MenuItem key="4"><Link to="22">菜单二2</Link></MenuItem>
-                </SubMenu>
-                <SubMenu key="sub4" title={<span><Icon type="setting" /><span>菜单三</span></span>}>
-                    <MenuItem key="5"><Link to="31">菜单三1</Link></MenuItem>
-                    <MenuItem key="6"><Link to="32">菜单三2</Link></MenuItem>
-                    <MenuItem key="7"><Link to="33">菜单三3</Link></MenuItem>
-                    <MenuItem key="8"><Link to="34">菜单三4</Link></MenuItem>
-                </SubMenu>
+                    <Menu.Item key="dashboard">
+                        <Link to={'/dashboard'}><Icon type="mobile" /><span className="nav-text">首页</span></Link>
+                    </Menu.Item>
+                    <SubMenu key="system" title={<span><Icon type="mail" /><span>系统管理</span></span>}>
+                        <MenuItem key="user"><Link to="/user">用户信息</Link></MenuItem>
+                        <MenuItem key="role"><Link to="/role">角色管理</Link></MenuItem>
+                    </SubMenu>
+                    <SubMenu key="sub4" title={<span><Icon type="switcher" /><span className="nav-text">页面</span></span>}>
+                        <Menu.Item key="/login"><Link to={'/login'}>登录</Link></Menu.Item>
+                        <Menu.Item key="/register"><Link to={'/register'}>注册</Link></Menu.Item>
+                        <Menu.Item key="/notFound"><Link to={'/404'}>404</Link></Menu.Item>
+                        <Menu.Item key="/shop"><Link to={'/shop'}>店铺</Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="/app/ui" title={<span><Icon type="scan" /><span className="nav-text">UI</span></span>}>
+                        <Menu.Item key="/app/ui/buttons"><Link to={'/app/ui/buttons'}>按钮</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/icons"><Link to={'/app/ui/icons'}>图标</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/spins"><Link to={'/app/ui/spins'}>加载中</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/modals"><Link to={'/app/ui/modals'}>对话框</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/notifications"><Link to={'/app/ui/notifications'}>通知提醒框</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/tabs"><Link to={'/app/ui/tabs'}>标签页</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/banners"><Link to={'/app/ui/banners'}>轮播图</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/wysiwyg"><Link to={'/app/ui/wysiwyg'}>富文本</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/drags"><Link to={'/app/ui/drags'}>拖拽</Link></Menu.Item>
+                        <Menu.Item key="/app/ui/gallery"><Link to={'/app/ui/gallery'}>画廊</Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="/app/animation" title={<span><Icon type="rocket" /><span className="nav-text">动画</span></span>}>
+                        <Menu.Item key="/app/animation/basicAnimations"><Link to={'/app/animation/basicAnimations'}>基础动画</Link></Menu.Item>
+                        <Menu.Item key="/app/animation/exampleAnimations"><Link to={'/app/animation/exampleAnimations'}>动画案例</Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="/app/table" title={<span><Icon type="copy" /><span className="nav-text">表格</span></span>} >
+                        <Menu.Item key="/app/table/basicTable"><Link to={'/app/table/basicTable'}>基础表格</Link></Menu.Item>
+                        <Menu.Item key="/app/table/advancedTable"><Link to={'/app/table/advancedTable'}>高级表格</Link></Menu.Item>
+                        <Menu.Item key="/app/table/asynchronousTable"><Link to={'/app/table/asynchronousTable'}>异步表格</Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="/app/form" title={<span><Icon type="edit" /><span className="nav-text">表单</span></span>}>
+                        <Menu.Item key="/app/basicForm"><Link to={'/app/form/basicForm'}>基础表单</Link></Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="/app/chart" title={<span><Icon type="area-chart" /><span className="nav-text">图表</span></span>}>
+                        <Menu.Item key="/app/chart/echarts"><Link to={'/app/chart/echarts'}>echarts</Link></Menu.Item>
+                        <Menu.Item key="/app/chart/recharts"><Link to={'/app/chart/recharts'}>recharts</Link></Menu.Item>
+                    </SubMenu>
                 </Menu>
             </Sider>
         );
--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Router, Route, IndexRedirect } from 'dva/router';
 import IndexPage from './routes/IndexPage';
-import Login from './routes/Login/Login';
-import Register from './routes/Register/Register';
-import Shop from './routes/Shop/Shop';
-import Dashboard from './routes/Dashboard/Dashboard';
+import Login from './routes/login/Login';
+import Register from './routes/register/Register';
+import Shop from './routes/shop/Shop';
+import Dashboard from './routes/dashboard/Dashboard';
+import NotFound from './components/NotFound';
 
 const RouterConfig = ({history, app}) => {
 
@@ -13,11 +14,39 @@ const RouterConfig = ({history, app}) => {
       		<Route path="/login" component={Login} />
             <Route path="/register" component={Register}/>
             <Route path="/" component={IndexPage} >
-                 <IndexRedirect to="shop"/>
-                 <Route path="shop" component={Shop} />
-                 <Route path="dashboard" component={Dashboard} />
+                <IndexRedirect to="dashboard"/>
+                <Route path="dashboard" component={Dashboard} />
+                <Route path="shop" component={Shop} />
+                <Route path={'form'}>
+                    <Route path={'basicForm'} component={BasicForm} />
+                </Route>
+                <Route path={'table'}>
+                    <Route path={'basicTable'} component={BasicTable} />
+                    <Route path={'advancedTable'} components={AdvancedTable} />
+                    <Route path={'asynchronousTable'} components={AsynchronousTable} />
+                </Route>
+                <Route path={'chart'}>
+                    <Route path={'echarts'} component={Echarts} />
+                    <Route path={'recharts'} component={Recharts} />
+                </Route>
+                <Route path={'ui'}>
+                    <Route path={'icons'} component={Icons} />
+                    <Route path={'buttons'} component={Buttons} />
+                    <Route path={'spins'} component={Spins} />
+                    <Route path={'modals'} component={Modals} />
+                    <Route path={'notifications'} component={Notifications} />
+                    <Route path={'tabs'} component={Tabs} />
+                    <Route path={'banners'} component={Banners} />
+                    <Route path={'wysiwyg'} getComponent={Wysiwyg} />
+                    <Route path={'drags'} component={Drags} />
+                    <Route path={'gallery'} component={Gallery} />
+                </Route>
+                <Route path={'animation'}>
+                    <Route path={'basicAnimations'} component={BasicAnimations} />
+                    <Route path={'exampleAnimations'} component={ExampleAnimations} />
+                </Route>
             </Route>
-
+            <Route path="*" component={NotFound}/>
     	</Router>
     );
 }
